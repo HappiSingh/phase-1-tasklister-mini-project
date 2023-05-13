f52d800df33dcb9dd148b6ae15a8711978cf3d45
Harpreet Singh <Harpreets1201@gmail.com>
2023-05-12 23:59:29 -0400

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   document.querySelector('form').addEventListener('submit', (e) => {
+//     e.preventDefault();
+//   })
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const newTaskForm = document.getElementById('create-task-form')
+  newTaskForm.addEventListener('submit', createNewTask)
+})
+
+function createNewTask(e) {
+  e.preventDefault(e)
+  const newTaskDescription = document.getElementById('new-task-description')
+  const newTask = document.createElement('li')
+  const btn = document.createElement('button')
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'X'
+  newTask.textContent = `${newTaskDescription.value} `
+  newTask.appendChild(btn).style.position = 'inherit'
+  appendTask(newTask)
+  e.target.reset()
+}
+
+function appendTask(newTask) {
+  document.getElementById('tasks').appendChild(newTask)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
